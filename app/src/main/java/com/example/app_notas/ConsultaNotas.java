ee4b21669ceef437901f26eb1efa14161e85433c
camilod29 <cadelgado@uniboyaca.edu.co>
2022-10-09 20:46:41 -0500

    Crear Las actividades principales
    jijija

---
+++ app/src/main/java/com/example/app_notas/ConsultaNotas.java
@@ -0,0 +1,14 @@
+package com.example.app_notas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConsultaNotas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consulta_notas);
+    }
+}
