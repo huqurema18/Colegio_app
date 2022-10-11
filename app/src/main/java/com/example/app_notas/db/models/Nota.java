ea671c8d7873da6e7eebddf1e061061871769871
camilod29 <cadelgado@uniboyaca.edu.co>
2022-10-11 17:06:06 -0500

    Update Nota.java

--- app/src/main/java/com/example/app_notas/db/models/Nota.java
+++ app/src/main/java/com/example/app_notas/db/models/Nota.java
@@ -1,4 +1,31 @@
 package com.example.app_notas.db.models;
 
 public class Nota {
+    private String materia;
+    private double calificacion;
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMateria() {
+        return materia;
+    }
+
+    public void setMateria(String materia) {
+        this.materia = materia;
+    }
+
+    public double getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(double calificacion) {
+        this.calificacion = calificacion;
+    }
 }
